9af81f74366976a40f7835e94896e2cc4637cc51
Stéphane BOUIN <sbouin@PHILO.INTRA>
2018-06-12 09:59:47 +0200

    page galerie

--- contact.php
+++ contact.php
@@ -5,6 +5,10 @@
     <title>page contact</title>
 </head>
 <body>
-    <h1>page de contact pour </h1>
+    <h1>page de contact pour attraper les mouches </h1>
+    <form methode="post">
+    <input type="text">
+    
+    </form>
 </body>
 </html>
