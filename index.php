286472aa45cbf5c489453b538dc0df9b9c82f397
Stéphane BOUIN <sbouin@PHILO.INTRA>
2018-06-12 09:40:35 +0200

    creation de la page d 'accueil

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gitv1</title>
+</head>
+<body>
+<h1>Page d accueil V1</h1>
+<p>Salut  les gens</p>
+
+    
+</body>
+</html>
